f92de1437d7c400a5980489c2ce1031908906e4e
root <root@dereck.com>
2017-01-30 20:30:44 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# kubernete_deploy
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: kubernete_deploy
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
